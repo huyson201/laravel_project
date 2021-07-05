522a578193d5d4bdb7f2eb9077bc7bb0916612b6
HuySon <huyson201@gmail.com>
2021-07-05 21:36:36 +0700

    complate search, add new, paginate

--- app/Http/Controllers/TrainerController.php
+++ app/Http/Controllers/TrainerController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Company;
 use App\Models\Trainer;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Http\Request;
 
 class TrainerController extends Controller
@@ -12,13 +13,33 @@ class TrainerController extends Controller
 
     public function index()
     {
-        $trainers = Trainer::paginate(12);
+        $trainers = Trainer::orderBy('trainer_id', 'DESC')->paginate(12);
+        return view('trainer.trainer-list', ['trainers' => $trainers]);
+    }
+
+    public function search(Request $request)
+    {
+        $request->validate([
+            'q'     => 'required'
+        ]);
+
+        $key = $request->q;
+
+        $trainers = Trainer::whereHas('company', function (Builder $query) use ($key) {
+            return $query->where('company_name', 'LIKE', "%{$key}%");
+        })->orWhere('trainer_name', 'Like', "%{$key}%")
+            ->orWhere('trainer_phone', 'Like', "%{$key}%")
+            ->orWhere('trainer_address',  'Like', "%{$key}%")
+            ->orderBy('trainer_id', 'DESC')->paginate(12)->appends($request->except('page'));
         return view('trainer.trainer-list', ['trainers' => $trainers]);
     }
 
     public function edit_view($id)
     {
         $trainer = Trainer::find($id);
+        if ($trainer == null) {
+            return abort(404, 'Trainer not found');
+        }
         $companies = Company::pluck('company_name', 'company_id');
         return view('trainer.trainer-form', ['trainer' => $trainer, 'companies' => $companies]);
     }
@@ -26,7 +47,7 @@ class TrainerController extends Controller
     public function edit(Request $request)
     {
         $request->validate([
-            'trainer_phone'    => 'regex:/^[0-9]{10}$/'
+            'trainer_phone'    => 'regex:/^[0-9]+$/'
         ]);
 
         $data = $request->only('trainer_name', 'trainer_phone', 'trainer_address', 'company_id');
@@ -40,9 +61,26 @@ class TrainerController extends Controller
         return view('trainer.trainer-form',  ['companies' => $companies]);
     }
 
+    public function create(Request $request)
+    {
+
+        $request->validate([
+            'trainer_name'      =>  'required',
+            'trainer_phone'     =>  'required|regex:/^[0-9]+$/',
+            'trainer_address'   =>  'required',
+            'company_id'        =>  'required|regex:/^[0-9]+$/'
+        ]);
+
+        $data = $request->all();
+        $trainer = new Trainer($data);
+        $trainer->save();
+
+        return redirect()->route('trainer.list')->with('add-message', "create trainer successfully!");
+    }
+
     public function delete($id)
     {
         Trainer::find($id)->delete();
-        return redirect()->route('trainer.list');
+        return redirect()->route('trainer.list')->with('delete-message', 'delete trainer successfully!');
     }
 }
