f1df261ed08208f59e3412cb48291051ed9f6f47
cms <80998461+kieuoanh231@users.noreply.github.com>
2021-07-08 22:45:27 +0700

    merge

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,10 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
-use Illuminate\Contracts\Hashing\Hasher;
-use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
